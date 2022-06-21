2c8b08d4a6cd6b5f6064ee874243a80a5812cc1c
baselm <basel.magableh@icloud.com>
2022-06-21 18:08:35 +0100

    first commit 2

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import React from "react";
+// import SignUp from './SignUp';
+import Signup2 from './components/Signup'
+
+function App() {
+  return (
+    <div className="App">
+      <React.Fragment>
+        <CssBaseline />
+   <Signup2 />
+
+      </React.Fragment>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signup.js
@@ -0,0 +1,112 @@
+import React, { Component } from 'react';
+import Grid from '@mui/material/Grid'
+import { Typography } from '@mui/material';
+import { Container } from '@mui/material';
+import Box from '@mui/material/Box'; 
+import TextField from '@mui/material/TextField';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel'; 
+import Button from '@mui/material/Button';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+ import CssBaseline from '@mui/material/CssBaseline';
+const theme = createTheme();
+class SignUp extends Component {
+
+    render() {  
+        return (
+            <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth='xs'>
+            <CssBaseline />
+                <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <Grid container spacing={2}>
+                        <Typography variant='h1' component='h5'>
+                            Sign Up
+                        </Typography>
+        
+            <Box component="form" noValidate sx={{mt:3}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    autoComplete='give-name'
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label = "First Name"
+                    autoFocus
+                    />
+                  
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <TextField
+                    autoComplete='last-name'
+                    name="lastName"
+                    required 
+                    fullWidth 
+                    id="lastName"
+                    label="Last Name"
+                    autoFocus
+                    />
+
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    autoComplete='email'
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField 
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    id="password"
+                    autoComplete='new-password'
+                    />
+                </Grid>
+            <Grid item xs={12}>
+            <FormControlLabel control={<Checkbox defaultChecked />} label="I want to receive inspiration, marketing promotions and updates via email." />
+
+
+            </Grid>
+            </Grid>
+            <Button 
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx = {{mt: 3, mb: 2}}>
+                Sign Up
+            </Button>
+            <Grid item>
+                <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                </Link>
+            </Grid>
+            </Box>
+
+                        </Grid>
+            </Box>
+
+            </Container>
+            </ThemeProvider>
+            
+          
+            
+        );
+    }
+}
+
+export default SignUp;
